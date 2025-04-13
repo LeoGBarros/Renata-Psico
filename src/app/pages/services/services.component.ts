34899b6f105b05d460e674eefdcc9a1e6623ea0f
Leonardo Barros <leoguedesbarros@gmail.com>
2025-04-12 22:37:38 -0300

    ajustes de responsividade e conectividade com links

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-services',
-  imports: [],
   templateUrl: './services.component.html',
-  styleUrl: './services.component.css'
+  styleUrls: ['./services.component.css']
 })
-export class ServicesComponent {
+export class ServicesComponent implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    const inicioLink = document.getElementById('scrollTopLink');
+    if (inicioLink) {
+      inicioLink.addEventListener('click', (event) => {
+        event.preventDefault();
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+      });
+    }
+  
+    // Animação ao scroll...
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('visible');
+        } else {
+          entry.target.classList.remove('visible');
+        }
+      });
+    }, { threshold: 0.1 });
+  
+    document.querySelectorAll('.animate-on-scroll').forEach((el) => {
+      observer.observe(el);
+    });
+  }
+  
+  
 }
