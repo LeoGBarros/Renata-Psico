7ad2652e5b71c80ad1e3afbe1f24328267e5f4c9
Leonardo Barros <leoguedesbarros@gmail.com>
2025-04-13 00:03:16 -0300

    ajuste paddig mobile

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -13,12 +13,10 @@ export class NavbarComponent implements AfterViewInit {
     const toggle = document.getElementById('menuToggle');
     const menu = document.getElementById('mobileMenu');
 
-    // ✅ Abre/fecha o menu ao clicar no botão hamburguer
     toggle?.addEventListener('click', () => {
       menu?.classList.toggle('open');
     });
 
-    // ✅ Fecha o menu ao clicar em qualquer link do menu mobile
     document.querySelectorAll('.mobile-menu-items a').forEach(link => {
       link.addEventListener('click', () => {
         menu?.classList.remove('open');
